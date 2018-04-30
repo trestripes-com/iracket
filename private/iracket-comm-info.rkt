868fa53b1258223f0883a7329797e0cf0b3a5529
Dean <deang1@scc-digsci.lancs.local>
2018-04-30 14:19:53 +0100

    Changes for comm_info_request message in Jupyter client protocol

--- private/iracket-comm-info.rkt
+++ private/iracket-comm-info.rkt
@@ -2,8 +2,23 @@
 
 (provide comm-info)
 
+;; comm_info_request / comm_info_reply
+;; A complete request / replyi template looks like:
 ;; comm_info_request
-;; replies with an empty dictionary for the comm_info_request
+;; content = {
+;;     # Optional, the target name
+;;     'target_name': str,
+;; }
+;; comm_info_reply
+;; content = {
+;;     # A dictionary of the comms, indexed by uuids.
+;;     'comms': {
+;;         comm_id: {
+;;             'target_name': str,
+;;         },
+;;     },
+;; }
+;;
+;; In this case, simply reply with an empty dictionary 
 (define comm-info
-  (hasheq
-   'comms ""))
+  (hasheq))
